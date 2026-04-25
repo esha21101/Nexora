89e8b9bf0942650231ef4d2ff0f09db82219c133
Esha Markam <esha21101@iiitnr.edu.in>
2026-04-26 00:07:10 +0530

    setup backend and database connection

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const app = express();
+const pool = require("./config/db");
 
 app.use(express.json());
+
+const eventRoutes = require("./routes/event");
+app.use("/event", eventRoutes);
+
+pool.query("SELECT NOW()", (err, res) => {
+  if (err) {
+    console.error("DB Error", err);
+  } else {
+    console.log("DB Connected:", res.rows[0]);
+  }
+});
 
 app.get("/", (req, res) => {
   res.send("Nexora API is running 🚀");
---
+++ routes/event.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const pool = require("../config/db");
+
+router.post("/", async (req, res) => {
+  const { user_id, event_type, metadata } = req.body;
+
+  try {
+    await pool.query(
+      "INSERT INTO events (user_id, event_type, metadata) VALUES ($1, $2, $3)",
+      [user_id, event_type, metadata]
+    );
+
+    res.json({ message: "Event stored successfully" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+module.exports = router;
