2808b0c9619470fc318397c6bad38243bc7e49af
Esha Markam <esha21101@iiitnr.edu.in>
2026-04-27 00:01:48 +0530

    connected render database

--- config/db.js
+++ config/db.js
@@ -1,11 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "nexora",
-  password: "1234", // use your password
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
